52718837f98c82c8a85813240b3c71a4b47175ef
vaibhav <vaibhav.khurana@hotmail.com>
2018-05-22 02:19:03 +0530

    Adding functionalities for multiple use cases and some changes in README

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # url_check
-Python script for checking url status using the status code.
-Usage: url_check.py -u[--url] url1, url2
-Example: 
+Python script for checking url status using the status code. It will not only notify the problem but will also suggest possible solutions, thus saving troubleshooting time. 
+
+Usage: 
+
+url_check.py -u[--url] url1, url2
+
+
+It will notify with possible problems in the below cases:
+
+1. If the ip given in the url is not reachable.
+2. If the port is not listening in the IP, thus connection is refused. 
+3. If there is a problem in the dns resolution
+
+
+Example Usage: 
 
 url_check.py -u https://google.com,https://facebook.com
--- url_status.py
+++ url_status.py
@@ -2,25 +2,27 @@
 import argparse
 import requests
 import sys
-unreachable=None
+unreachable=[]
 def status(url):
     try:
-        req=requests.get(url)
+        req=requests.get(url,timeout=5)
         if req.status_code!=200:
             unreachable.append(url)
-        #return req.status_code
+    except requests.exceptions.ConnectionError:
+        unreachable.append("%s is giving connection refused, check dns and port is listening" %url)
+    except TimeoutError:
+        unreachable.append("%s is timed out, check for accessbility of the url" %url)
     except Exception:
-        print("Something is fisshy with "+url)
-        sys.exit(3)
+        print("Something is fisshy with %s, have you added http:// or https:// in the url" %url)
+
 if __name__=="__main__":
     parser=argparse.ArgumentParser(description="Get the url list for checking response code")
     parser.add_argument("-u", "--url", help="Enter the url's seprated by comma you wish to check",required=True)
     urls=parser.parse_args().url.split(',')
-    #print(urls)
     for url in urls:
         status(url)
     if unreachable:
-        print("The following url's are DOWN: " +unreachable)
+        print("The following url's are DOWN: " + str(unreachable) )
         sys.exit(2)
     else:
         print("All Good!!")
